f64b9882261e5c3bbf11a243e6b9a93da0b4f876
inspiringrai <vishalrai447@gmail.com>
2022-06-06 20:40:14 +0530

    Created Services

---
+++ src/main/java/com/demo/EMS/Services/DepartmentService.java
@@ -0,0 +1,33 @@
+package com.demo.EMS.Services;
+
+import com.demo.EMS.Repository.DepartmentRepo;
+import com.demo.EMS.beans.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface DepartmentService {
+    // fetching all department
+    List<Department> getAllDepartments();
+
+    // fetching department by id
+    Department getDepartment(int id);
+
+    // inserting department
+    void addDepartment(Department d);
+
+    // updating department by id
+    void updateDepartment(Department d, int id);
+
+    // deleting all departments
+    void deleteAllDepartment();
+
+    // deleting department by id
+    void deleteDepartmentByID(int id);
+
+    //patching/updating department by id
+    void patchDepartment(Department d, int id) ;
+
+
+}
---
+++ src/main/java/com/demo/EMS/Services/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.demo.EMS.Services;
+
+import com.demo.EMS.beans.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+   // fetching all employee
+   List<Employee> getAllEmployees();
+
+    // fetching employee by id
+    Employee getEmployee(int id);
+
+    // inserting employee
+    void addEmployee(Employee e) ;
+
+    // updating employee by id
+    void updateEmployee(Employee emp, int id);
+
+    // deleting all employees
+    void deleteAllEmployees();
+
+    // deleting employee by id
+    void deleteEmployeeByID(int id);
+
+    //patching/updating employee by id
+    void patchEmployee(Employee emp, int id);
+}
